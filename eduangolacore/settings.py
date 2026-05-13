1f48eba3960e626595ef7aca6b1a5b1a4e7a714b
00charles <muquissicarlos@gmail.com>
2026-05-13 17:22:40 -0300

    Trocar SMTP para Brevo - resolve bloqueio de porta do Render

--- eduangolacore/settings.py
+++ eduangolacore/settings.py
@@ -291,14 +291,14 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.privateemail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_TIMEOUT = 15
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='info@edukangola.com')
 
 
 
